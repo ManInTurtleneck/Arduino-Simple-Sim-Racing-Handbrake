b5212b2e2a9cf17b51a45807f22bb6ad68809f23
Django Schmidt <86383936+ManInTurtleneck@users.noreply.github.com>
2025-09-06 19:01:53 -0600

    Handbrake
    Handbrake

---
+++ Handbrake/Handbrake.ino
@@ -0,0 +1,29 @@
+#include <Joystick.h>
+
+const uint8_t POT_PIN = A0; // change to A1/A2/A3 if soldered to different analog pin
+const bool INVERT = true;  // set true if direction is backwards
+const int start = 900;     // adjust value to increase distance you need to pull handbrake for start of activation
+const int end = 900;       // adjust value to reduce distance you need to pull handbrake for full activation
+
+// disables unnecessary joystick library features
+Joystick_ Joystick(
+  JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
+  0, 0,                       // buttons, hats
+  false, false, false,        // X,Y,Z
+  false, false, false,        // Rx,Ry,Rz
+  false, true,  false,  false // Rudder, Throttle, Accelerator, Steering
+);
+
+void setup() {
+  Joystick.setThrottleRange(0, end); 
+  Joystick.begin(false);
+}
+
+void loop() {
+  int raw = analogRead(POT_PIN);
+  if (INVERT) raw = start - raw; 
+    else raw -= (1023 - start);
+  Joystick.setThrottle(raw);
+  Joystick.sendState();
+  delay(2);
+}
